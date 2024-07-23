dd301466fe7bd3c3974f5dfee1d5bb93ee28bbae
BRYAN HOLBROOK <BRYANDHOLBROOK@GMAIL.com>
2024-07-23 14:32:28 -0700

    C2 Methods: Mean Temp in (F). Max Wind Speed in (Mph). Precip Sum in (In).

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import os
+from weather_data import WeatherData
+
+#unfinished methods
+wdata_ins = WeatherData()
+
+#unfinished methods
+wdata_ins.mean_temp(#api dat)
+
+wdata_ins.max_wind_speed(#api data)
+
+wdata_ins.precipitation_sum(#api data)
+
 
 print("Hi There")
